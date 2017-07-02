082493875680dd3a7f3e10b6de91bbd830901f5c
ikkemaniac <ikkemaniac@localhost>
2017-07-02 11:18:46 +0200

    modify sha256 check

--- scripts/download_update.sh
+++ scripts/download_update.sh
@@ -22,26 +22,33 @@ archive_file="http://www.domoticz.com/download.php?channel=${CHANNEL}&type=relea
 checksum_file="http://www.domoticz.com/download.php?channel=${CHANNEL}&type=checksum&system=${OS}&machine=${MACH}"
 
 # Download checksum
-wget -q "${checksum_file}" -O update.tgz.sha256sum
-if [ $? -ne 0 ]
-then
-        echo "Error downloading checksum file!...";
-        exit 1
-fi
+echo "wget -q \"${checksum_file}\" -O update.tgz.sha256sum"
+#~ wget -q "${checksum_file}" -O update.tgz.sha256sum
+#~ if [ $? -ne 0 ]
+#~ then
+        #~ echo "Error downloading checksum file!...";
+        #~ exit 1
+#~ fi
 
 # Download archive file
-wget -q "${archive_file}" -O update.tgz
-if [ $? -ne 0 ]
-then
-        echo "Error downloading archive file!...";
-        exit 1
-fi
+echo "wget -q \"${archive_file}\" -O update.tgz"
+#~ echo "wget -q \"${archive_file}\" -O update.tgz"
+#~ wget -q "${archive_file}" -O update.tgz
+#~ wget -q "${archive_file}" -O update.tgz
+#~ if [ $? -ne 0 ]
+#~ then
+        #~ echo "Error downloading archive file!...";
+        #~ exit 1
+#~ fi
 
 # Check archive
 if [ -f update.tgz.sha256sum ];
 then
   #Check archive against checksum!
-  valid=$(LC_ALL=C sha256sum -c update.tgz.sha256sum | grep update.tgz | cut -d':' -f2 | tr -d ' ')
+  #~ echo "LC_ALL=C sha256sum -c update.tgz.sha256sum | grep update.tgz | cut -d':' -f2 | tr -d ' ')"
+  #~ valid=$(LC_ALL=C sha256sum -c update.tgz.sha256sum | grep update.tgz | cut -d':' -f2 | tr -d ' ')
+  echo "LC_ALL=C cat update.tgz.sha256sum | awk '{print $1\"  update.tgz\"}' | sha256sum -c | grep update.tgz | cut -d':' -f2 | tr -d ' '"
+  valid=$(LC_ALL=C cat update.tgz.sha256sum | awk '{print $1"  update.tgz"}' | sha256sum -c | grep update.tgz | cut -d':' -f2 | tr -d ' ')
   if [ $valid != "OK" ]
   then
         echo "Archive checksum mismatch !";
